2e3a0e97d0e404e48cf65f63d2e141c9bbca1836
Miroslav Tynovsky <tynovsky@seznam.cz>
2019-12-07 18:32:40 +0100

    7b

--- 07/Machine.pm
+++ 07/Machine.pm
@@ -2,6 +2,7 @@ package Machine;
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 sub new {
     my ($class, $program, $input, $output) = @_;
@@ -18,7 +19,8 @@ sub new {
 
 sub run {
     my ($self) = @_;
-    $self->{execution} = 'run'
+    return if $self->{execution} eq 'halt';
+    $self->{execution} = 'run';
     while ($self->{execution} eq 'run') {
         $self->run_instruction();
     }
@@ -60,18 +62,115 @@ sub run_instruction {
 	99 => 0,
     };
 
-    my $opcode = opcode($memory, $i);
+    my $opcode = $self->opcode();
     my $op = $ops->{$opcode};
     my $number_of_args = $number_of_args_for->{$opcode};
-    my $args = args($memory, $i, $number_of_args);
+    my $args = $self->args($number_of_args);
     my $write_address;
     if ($writes->{$opcode}) {
-	$write_address = $memory->[$i + $number_of_args + 1];
+	$write_address = $self->{memory}[$self->{i} + $number_of_args + 1];
     }
-    # say join(',', @{$memory}[$i .. $i + $number_of_args + $writes->{$opcode}]);
-    # say $memory->[$i], " ", $opcode, "(", join(',',@$args), ")";
-    # if ($writes->{$opcode}) {
-    #     say "\t--> ", $output_address;
-    # }
-    return $op->($memory, $i, $args, $write_address, $in, $out)
+    return $op->($self, $args, $write_address)
 }
+
+sub read {
+    my ($self, $i) = @_;
+    $i //= $self->{i};
+    return $self->{memory}[$i]
+}
+
+sub write {
+    my ($self, $i, $val) = @_;
+    $self->{memory}[$i] = $val;
+}
+
+sub opcode {
+    my $self = shift;
+    return $self->{memory}->[$self->{i}] % 100;
+}
+
+sub args {
+    my ($self, $number_of_args) = @_;
+    my $opcode = $self->opcode();
+    my $mode = ($self->read() - $opcode) / 100;
+    my @mode = reverse(split(//, $mode)) if $mode;
+    my @args = @{$self->{memory}}[($self->{i} + 1) .. ($self->{i} + $number_of_args)];
+    for my $j (0 .. $#args) {
+	$args[$j] = $self->{memory}[$args[$j]] if ! $mode[$j]
+    }
+    return [@args];
+}
+
+sub write_address {
+    my ($memory, $i, $index_offset) = @_;
+    return if ! $index_offset;
+    return $memory->[$i + $index_offset]
+}
+
+sub add {
+    my ($self, $args, $write_address) = @_;
+    $self->write($write_address, $args->[0] + $args->[1]);
+    $self->{i} += 4
+}
+
+sub mul {
+    my ($self, $args, $write_address) = @_;
+    $self->write($write_address, $args->[0] * $args->[1]);
+    $self->{i} += 4
+}
+
+sub input {
+    my ($self, undef, $write_address) = @_;
+    if (@{$self->{input}} == 0) {
+	$self->{execution} = 'block';
+	return
+    }
+    $self->write($write_address, shift @{$self->{input}});
+    $self->{i} += 2;
+}
+
+sub output {
+    my ($self, $args) = @_;
+    push @{$self->{output}}, $args->[0];
+    # say "OUT: " . $args->[0];
+    $self->{i} += 2;
+}
+
+sub jump_if_true {
+    my ($self, $args) = @_;
+    if ($args->[0]) {
+	$self->{i} = $args->[1];
+    }
+    else {
+	$self->{i} += 3;
+    }
+}
+
+sub jump_if_false {
+    my ($self, $args) = @_;
+    if (! $args->[0]) {
+	$self->{i} = $args->[1]
+    }
+    else {
+	$self->{i} += 3
+    }
+}
+
+sub less_than {
+    my ($self, $args, $write_address) = @_;
+    $self->write($write_address, ($args->[0] < $args->[1]) ? 1 : 0);
+    return $self->{i} += 4
+}
+
+sub equals {
+    my ($self, $args, $write_address) = @_;
+    $self->write($write_address, ($args->[0] == $args->[1]) ? 1 : 0);
+    return $self->{i} += 4
+}
+
+sub halt {
+    my ($self) = @_;
+    $self->{execution} = 'halt';
+}
+
+1;
