ee57340ffd2473a829944d00ad2f3e9f974bd2cc
Miroslav Tynovsky <tynovsky@seznam.cz>
2019-12-09 19:30:52 +0100

    9 cleanup

--- 09/Machine.pm
+++ 09/Machine.pm
@@ -2,10 +2,11 @@ package Machine;
 
 use strict;
 use warnings;
+use feature 'signatures';
+no warnings 'experimental::signatures';
 use Data::Dumper;
 
-sub new {
-    my ($class, $program, $input, $output) = @_;
+sub new($class, $program, $input, $output) {
     my $self = {
         memory => [ split /,/, $program ],
         relative_base => 0,
@@ -14,11 +15,10 @@ sub new {
         output => $output,
         execution => 'run',
     };
-    bless $self, $class
+    return bless $self, $class
 }
 
-sub run {
-    my ($self) = @_;
+sub run($self) {
     return if $self->{execution} eq 'halt';
     $self->{execution} = 'run';
     while ($self->{execution} eq 'run') {
@@ -26,15 +26,14 @@ sub run {
     }
 }
 
-sub run_instruction {
-    my ($self) = @_;
+sub run_instruction($self) {
     my $ops = {
         1 =>  [\&add, 3],
         2 =>  [\&mul, 3],
         3 =>  [\&input, 1],
-        4 =>  [\&output, 2],
-        5 =>  [\&jump_if_true, 3],
-        6 =>  [\&jump_if_false, 3],
+        4 =>  [\&output, 1],
+        5 =>  [\&jump_if_true, 2],
+        6 =>  [\&jump_if_false, 2],
         7 =>  [\&less_than, 3],
         8 =>  [\&equals, 3],
 	9 =>  [\&relative_base_offset, 1],
@@ -47,44 +46,31 @@ sub run_instruction {
     return $op->($self, @$args)
 }
 
-sub args {
-    my ($self, $number_of_args) = @_;
+sub args($self, $number_of_args) {
     my $mode = int($self->{memory}[$self->{i}] / 100);
     my @args = ();
     for my $j (0 .. $number_of_args - 1) {
         my $arg = $self->{memory}[$self->{i} + $j + 1] // 0;
         my $m = $mode % 10;
         $mode = ($mode - $m) / 10;
-        if (!$m) {
-            push @args, \($self->{memory}[$arg]);
-            next;
-        }
-        if ($m == 1) {
-            push @args, \(my $tmp = $arg);
-            next;
-        }
-        if ($m == 2) {
-            push @args, \($self->{memory}[$self->{relative_base} + $arg]);
-            next;
-        }
+        push @args, \($self->{memory}[$arg]) if !$m;
+        push @args, \(my $tmp = $arg) if $m == 1;
+        push @args, \($self->{memory}[$self->{relative_base} + $arg]) if $m == 2;
     }
     return \@args;
 }
 
-sub add {
-    my ($self, $x, $y, $z) = @_;
+sub add($self, $x, $y, $z) {
     $$z = $$x + $$y;
     $self->{i} += 4
 }
 
-sub mul {
-    my ($self, $x, $y, $z) = @_;
+sub mul($self, $x, $y, $z) {
     $$z = $$x * $$y;
     $self->{i} += 4
 }
 
-sub input {
-    my ($self, $x) = @_;
+sub input($self, $x) {
     if (@{$self->{input}} == 0) {
         $self->{execution} = 'block';
         return
@@ -93,42 +79,35 @@ sub input {
     $self->{i} += 2;
 }
 
-sub output {
-    my ($self, $x) = @_;
+sub output($self, $x) {
     push @{$self->{output}}, $$x;
     $self->{i} += 2;
 }
 
-sub jump_if_true {
-    my ($self, $x, $y) = @_;
+sub jump_if_true($self, $x, $y) {
     $self->{i} = $$x ? $$y : $self->{i} + 3;
 }
 
-sub jump_if_false {
-    my ($self, $x, $y) = @_;
+sub jump_if_false($self, $x, $y) {
     $self->{i} = !$$x ? $$y : $self->{i} + 3;
 }
 
-sub less_than {
-    my ($self, $x, $y, $z) = @_;
+sub less_than($self, $x, $y, $z) {
     $$z = ($$x < $$y) ? 1 : 0;
     $self->{i} += 4
 }
 
-sub equals {
-    my ($self, $x, $y, $z) = @_;
+sub equals($self, $x, $y, $z) {
     $$z = ($$x == $$y) ? 1 : 0;
     $self->{i} += 4
 }
 
-sub relative_base_offset {
-    my ($self, $x) = @_;
+sub relative_base_offset($self, $x) {
     $self->{relative_base} += $$x;
     $self->{i} += 2
 }
 
-sub halt {
-    my ($self) = @_;
+sub halt($self) {
     $self->{execution} = 'halt';
 }
 
